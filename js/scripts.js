ddcbb0bced064d4d473c9c714c1fd7cff4cd8cc6
MaryAnne45 <mary.jereh@gmail.com>
2018-06-19 21:51:58 +0300

    add a modal effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,50 @@
-$(document).ready(function() {
-  $( '.menu-btn' ).click(function(){
-  $('.responsive-menu').toggleClass('expand')
-});
-});
-	function openPage(pageName,elmnt,color) {
-    var i,tabcontent,tablinks;
-    tabcontent =
-    document.getElementsByClassName("tabcontent");
-    for (i=0;i< tabcontent.length;i++) {
-      tabcontent[i].style.display="none";
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
     }
-    tablinks=document.getElementsByClassName("tablink");
-    for (i=0; i< tablinks.length; i++){
-      tablinks[i].style.backgroundColor="";
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
     }
-    document.getElementById(pageName).style.display="block";
-    elmnt.style.backgroundColor = color;
-  }
-  document.getElementById("defaultOpen").click();
-console.error("Error!!");
-console.log("hello world");
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+})(jQuery); // End of use strict
